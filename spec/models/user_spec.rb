5519c716649ab1414da2dac330fc76534701b405
haroldtreen <haroldtreen@gmail.com>
2014-06-22 01:45:01 -0400

    HaroldT: Add validation for name being unique for a given dispute

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,10 @@ describe User do
 
 		expect(incomplete_user).to_not be_valid
 	end
+
+	it 'should have a unique name for the dispute' do
+		dispute = create(:dispute)
+		user_1 = create(:user, name: "Foo", dispute_id: dispute.id)
+		expect(build(:user, name: "Foo", dispute_id: dispute.id)).to_not be_valid
+	end
 end
